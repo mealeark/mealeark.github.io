c980ea946377cf3933082451dac821dbb387b58e
Mealear <mileyy.mk@gmail.com>
2018-08-09 16:13:16 -0700

    Add Landing component

---
+++ src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from '../style/landing.less';
+
+const Landing = () => (
+  <div className={style.landing}>
+    <div className={style.one}/>
+    <div className={style.two}/>
+    <div className={style.three}/>
+    <div className={style.four}>
+      <h2>welcome!</h2>
+    </div>
+  </div>
+);
+
+export default Landing;
