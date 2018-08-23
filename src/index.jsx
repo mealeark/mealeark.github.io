348d970c4468c4d96cc8796c9f4d5ac0dd31f2a2
Mealear <mileyy.mk@gmail.com>
2018-08-23 14:35:59 -0700

    Remove unused dependencies

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppWithRouter from './components/App.jsx';
-
-ReactDOM.render(<Router><AppWithRouter /></Router>, document.getElementById('app'));
